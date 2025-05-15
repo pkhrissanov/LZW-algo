3012d8bffa6c3ce106e855cef4c4105d954fbc41
Peter Khrissanov <pkhrissanov@gmail.com>
2025-05-14 18:59:22 -0700

    debugged code

---
+++ encoding scipt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void initial_dict();
+
+int main(){
+
+    initial_dict();
+
+
+}
+
+
+
+void initial_dict() {
+    for(int i = 0; i <= 127; i++) {
+        char *asci_char = malloc(2 * sizeof(char));
+        asci_char[0] = (char)i;
+        asci_char[1] = '\0';
+        printf("the character %s has the ascii value %d\n", asci_char, i);
+    }
+}
+
+/* to do 
+
+write initial dictionary function
+figure out how i am storing everything 
+write store key and value function
+write input stream for hash map 
+hash map function for new strings? 
+
+
+
+*/
