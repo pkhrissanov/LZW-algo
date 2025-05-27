6226423d1d69bf1fe2de0f9e4bc8e9cd6f32f919
Peter <pkhrissanov@gmail.com>
2025-05-26 17:12:31 -0700

    testing initial dictionary printing

--- lzw-algo.c
+++ lzw-algo.c
@@ -4,171 +4,132 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define MAX_NAME 256
 #define TABLE_SIZE 541
 #define WORD_LEN 100
 
 
-
-unsigned long hash(unsigned char *str)
-{
+unsigned long hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
-
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
     return hash;
 }
 
+
 typedef struct node {
-    char word[100];
+    char word[WORD_LEN];
     unsigned long hashValue;
-    int index;
+    int index; 
     struct node* next;
 } node;
 
-node* create_node(const char* word) {
+
+node* create_node(const char* word, int code) {
     node* new_node = malloc(sizeof(node));
-    if (new_node == NULL) {
+    if (!new_node) {
         perror("Memory allocation failed");
         exit(1);
     }
-    strncpy(new_node->word, word, strlen(word));
-    new_node->word[strlen(word)] = '\0'; 
-    new_node-> hashValue = hash(word);
-    new_node-> index = new_node->hashValue % TABLE_SIZE;
+    strncpy(new_node->word, word, WORD_LEN - 1);
+    new_node->word[WORD_LEN - 1] = '\0';
+    new_node->hashValue = hash((unsigned char*)word);
+    new_node->index = code;
     new_node->next = NULL;
     return new_node;
 }
 
 
-void dict_init(node* hashTable[], int* bucketCounts, int* nextCode) {
-    for (int i = 0; i < 256; i++) {
-        char ascii_str[2];
-        ascii_str[0] = (char)i;
-        ascii_str[1] = '\0';
-
-        node* new_entry = malloc(sizeof(node));
-        if (new_entry == NULL) {
-            perror("Memory allocation failed");
-            exit(1);
-        }
-
-        strncpy(new_entry->word, ascii_str, 2);
-        new_entry->hashValue = hash((unsigned char*)ascii_str);
-        new_entry->index = i;
-        new_entry->next = NULL;
-
-        int table_index = new_entry->hashValue % TABLE_SIZE;
-        new_entry->next = hashTable[table_index];
-        hashTable[table_index] = new_entry;
-
-        bucketCounts[table_index]++;
-    }
-
-    *nextCode = 256;
+void insert_to_dict(node* hashTable[], int* bucketCounts, const char* word, int code) {
+    node* new_node = create_node(word, code);
+    int table_index = new_node->hashValue % TABLE_SIZE;
+    new_node->next = hashTable[table_index];
+    hashTable[table_index] = new_node;
+    bucketCounts[table_index]++;
 }
 
+
+
+void dict_init(node* hashTable[], int* bucketCounts, int* nextCode) {
+    for (int i = 0; i < 256; i++) {
+        char str[2] = {(char)i, '\0'};
+        insert_to_dict(hashTable, bucketCounts, str, i);
+    }
+    *nextCode = 256;
+}
 
 
 void print_bucket_counts(int *bucketCounts) {
     for (int i = 0; i < TABLE_SIZE; i++) {
         printf("Bucket %d: %d item(s)\n", i, bucketCounts[i]);
     }
+    
 }
 
 
-void lzw_algo(const char* word, node){
 
-}
-
-// succesful git 
-/*
-hello world
-
-
-*/
-
-
-int main(){
-
+int main() {
+    char filename[256];
     printf("Enter file name to read: ");
-    char buffer[1024];
-    scanf("%s", buffer);
-    FILE *fileptr;
-    fileptr = fopen(buffer, "r");
+    scanf("%255s", filename);
 
-    if(fileptr == NULL){
-        printf("file name invalid");
+    FILE* fileptr = fopen(filename, "r");
+    if (!fileptr) {
+        printf("File name invalid.\n");
+        return 1;
+    } else {
+        printf("File properly opened.\n");
     }
-    else{
-        printf("file properly opened");
-    }
-
 
 
     node* hashTable[TABLE_SIZE] = {NULL};
-    int bucketCounts[TABLE_SIZE] = {0}; 
+    int bucketCounts[TABLE_SIZE] = {0};
+    int nextCode;
 
+    dict_init(hashTable, bucketCounts, &nextCode);
 
-    
-
-
-
-    char word[WORD_LEN];
-
-    if (fscanf(fileptr, "%99s", word) != 1) {
-        printf("No words in file.\n");
-        fclose(fileptr);
-        return 1;
-    }
-
-
-    
-    node* head = create_node(word);
-    node* current = head;
-
-    while (fscanf(fileptr, "%99s", word) == 1) {
-        current->next = create_node(word);
-        hashTable[current->index] = current;
-        bucketCounts[current->index]++;
-        current = current->next;
-    }
+    //lzw_algo(fileptr, hashTable, bucketCounts, nextCode);
 
     fclose(fileptr);
 
+    print_bucket_counts(bucketCounts);
+    for (int i = 0; i < 256; i++) {
+    char ascii_str[2] = { (char)i, '\0' };
 
-    printf("Words in the list:\n");
-    current = head;
+
+    //test to see where the initial characters are gettitng printed too 
+
+    unsigned long h = hash((unsigned char*)ascii_str);
+    int index = h % TABLE_SIZE;
+    node* current = hashTable[index];
+
     while (current) {
-        printf("word: %s        ", current->word);
-        printf("hashed value: %u        ", current->hashValue);
-        printf("table index: %d\n", current->index);
+        if (strcmp(current->word, ascii_str) == 0) {
+            printf("ASCII '%c' (%d) => code %d (bucket %d)\n",
+                (i >= 32 && i < 127) ? i : '.', i, current->index, index);
+            break;
+        }
         current = current->next;
     }
-    print_bucket_counts(bucketCounts);
+
+    if (!current) {
+        printf("Missing ASCII code %d in dictionary.\n", i);
+    }
+}
 
 
 
-    current = head;
-    while (current) {
-        node* temp = current;
-        current = current->next;
-        free(temp);
+
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        node* current = hashTable[i];
+        while (current) {
+            node* temp = current;
+            current = current->next;
+            free(temp);
+        }
     }
 
     return 0;
 }
-
-/* 
-algo planning - 
-have a buffer - stores and compares the characters
-
-
-
-
-
-
-
-*/
