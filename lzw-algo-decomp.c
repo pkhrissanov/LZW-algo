2cca62fdeda16d5d7343ee4b7feb191980939332
Peter <pkhrissanov@gmail.com>
2025-06-05 15:11:19 -0700

    morning work and some new stuff. finished decompression, have a seg fauilt somehwere to find - not sure where yet

--- lzw-algo-decomp.c
+++ lzw-algo-decomp.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
 
 #define MAX_DICT_SIZE 4096
+#define ASCII_TABLE_SIZE 256
 
 char* dictionary[MAX_DICT_SIZE];
 
@@ -30,33 +33,76 @@ void output_string(FILE *out, const char *str) {
 
 
 
+int read_next_code(FILE *fp, int *code) {
+    static int buffer = 0;
+    static int bits_in_buffer = 0;
+
+    while (bits_in_buffer < 12) {
+        int next_byte = fgetc(fp);
+        if (next_byte == EOF) {
+            if (bits_in_buffer == 0) return 0; 
+            else {
+                
+                buffer <<= (12 - bits_in_buffer);
+                *code = buffer & 0xFFF;
+                bits_in_buffer = 0;
+                buffer = 0;
+                return 1;
+            }
+        }
+        buffer = (buffer << 8) | next_byte;
+        bits_in_buffer += 8;
+    }
+  bits_in_buffer -= 12;
+    *code = (buffer >> bits_in_buffer) & 0xFFF;
+
+    return 1;
+}
+
+
+
+
 int main(){
 
-    char filename[256];
-    debug();
-    printf("Enter file name to decompres: ");
-    scanf("%255s", filename);
+init_dictionary();
 
-    FILE* fileptr = fopen(filename, "rb");
-    if (!fileptr) {
-        printf("File name invalid.\n");
-        return 1;
-    } else {
-        printf("File properly opened.\n");
-    }
+char filename[256];
+char outputname[256];
 
+printf("Enter file name to decompress: ");
+scanf("%255s", filename);
 
+FILE* input = fopen(filename, "rb");
+if (!input) {
+    printf("File name invalid.\n");
+    return 1;
+}
+
+printf("Enter output file name: ");
+scanf("%255s", outputname);
+
+FILE* output = fopen(outputname, "w");
+if (!output) {
+    printf("File name invalid.\n");
+    fclose(input);
+    return 1;
+}
 
 int next_code = ASCII_TABLE_SIZE; 
 int previous_code, current_code;
 char *previous_string, *current_string;
 
+if (!read_next_code(input, &previous_code)) {
+    printf("Empty or corrupt file.\n");
+    fclose(input);
+    fclose(output);
+    return 1;
+}
 
 previous_string = dictionary[previous_code];
 output_string(output, previous_string);
 
-// Main loop
-while (read_next_code(&current_code, input)) { 
+while (read_next_code(input, &current_code)) {
     if (current_code < next_code && dictionary[current_code]) {
         current_string = dictionary[current_code];
     } else {
@@ -78,25 +124,18 @@ while (read_next_code(&current_code, input)) {
         dictionary[next_code++] = new_entry;
     }
 
-    previous_string = current_string;
+    if (current_code >= next_code) {
+        free(current_string); 
+    }
+
+    previous_string = dictionary[current_code];  
     previous_code = current_code;
 }
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
+free_dictionary(next_code);
+fclose(input);
+fclose(output);
+return 0;
 }
 
 
